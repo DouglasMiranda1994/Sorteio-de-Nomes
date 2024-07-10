5b97ae93f2d2a7b89f16ccd38da6d3742748fc1b
Douglas Miranda <dougurasu104@hotmail.com>
2024-07-10 10:08:22 -0300

    App de sorteio de nomes

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+const btnSortear = document.querySelector('.botaoSortear');
+const textArea = document.querySelector('textarea');
+const tagResultado= document.querySelector('.resultado p');
+const popUpResultado = document.querySelector('.resultado');
+const btnFechar = document.querySelector('.fechar');
+
+function pegarDadosFormularios(){
+    const valorCampo = textArea.value
+    const listaNomes = valorCampo.split(",")
+    const tamanhoArray = listaNomes.length
+    const posicaoNome = gerarNumeroAleatorio(tamanhoArray)
+    const nome  = listaNomes[posicaoNome]
+    mostrarResultado(nome)
+
+    textArea.value = ""
+
+}
+
+btnSortear.addEventListener("click", pegarDadosFormularios)
+
+function gerarNumeroAleatorio(tamanhoArray){
+    return Math.floor(Math.random() * (tamanhoArray - 0) + 0)
+}
+
+function mostrarResultado(nome){
+    tagResultado.innerText = `Resultado: ${nome}`
+    popUpResultado.classList.add("mostrarResultado")
+}
+
+function fecharPopUp(){
+    popUpResultado.classList.remove("mostrarResultado")
+}
+
+btnFechar.addEventListener("click", fecharPopUp)
+
+
+
